67d89c819c674732126863bfd3b1e9599f5e4525
Дима Буглак <jlynter@gmail.com>
2023-04-22 22:20:47 +0300

    testt

--- main.py
+++ main.py
@@ -1,41 +1,79 @@
+import sys
+
+# Словник для зберігання контактів
 contacts = {}
 
-while True:
-    command = input("What can I do for you? ").lower()
+# Функція обробник для команди "hello"
+def hello():
+    print("How can I help you?")
 
-    if command == "hello":
-        print("How can I help you?")
+# Функція обробник для команди "add"
+def add_contact(name, phone):
+    contacts[name] = phone
+    print(f"Contact {name} with phone number {phone} has been added")
 
-    elif command.startswith("add "):
-        name, phone = command.split()[1:]
+# Функція обробник для команди "change"
+def change_phone(name, phone):
+    if name in contacts:
         contacts[name] = phone
-        print(f"Added {name} with phone number {phone}")
-
-    elif command.startswith("change "):
-        name, phone = command.split()[1:]
-        if name in contacts:
-            contacts[name] = phone
-            print(f"Changed phone number for {name} to {phone}")
-        else:
-            print(f"{name} not found")
-
-    elif command.startswith("phone "):
-        name = command.split()[1]
-        if name in contacts:
-            print(f"{name}'s phone number is {contacts[name]}")
-        else:
-            print(f"{name} not found")
-
-    elif command == "show all":
-        if contacts:
-            for name, phone in contacts.items():
-                print(f"{name}: {phone}")
-        else:
-            print("No contacts found")
-
-    elif command in ("good bye", "close", "exit"):
-        print("Good bye!")
-        break
-
+        print(f"Phone number for {name} has been changed to {phone}")
     else:
-        print("Unknown command")
+        print(f"{name} is not in contacts")
+
+# Функція обробник для команди "phone"
+def show_phone(name):
+    if name in contacts:
+        print(f"The phone number for {name} is {contacts[name]}")
+    else:
+        print(f"{name} is not in contacts")
+
+# Функція обробник для команди "show all"
+def show_all():
+    if not contacts:
+        print("There are no contacts")
+    else:
+        print("Contacts:")
+        for name, phone in contacts.items():
+            print(f"{name}: {phone}")
+
+# Декоратор для перехоплення помилок
+def handle_errors(func):
+    def wrapper(*args):
+        try:
+            func(*args)
+        except Exception as e:
+            print(f"Error: {e}")
+    return wrapper
+
+# Функція парсера команд
+def parse_command(command):
+    tokens = command.strip().split()
+    if not tokens:
+        return None, None
+    if tokens[0].lower() == "hello":
+        return hello, None
+    if tokens[0].lower() == "add" and len(tokens) == 3:
+        return add_contact, (tokens[1], tokens[2])
+    if tokens[0].lower() == "change" and len(tokens) == 3:
+        return change_phone, (tokens[1], tokens[2])
+    if tokens[0].lower() == "phone" and len(tokens) == 2:
+        return show_phone, (tokens[1],)
+    if tokens[0].lower() == "show" and tokens[1].lower() == "all":
+        return show_all, None
+    return None, None
+
+# Основна функція бота
+@handle_errors
+def main():
+    while True:
+        command = input("> ")
+        if command.lower() == "exit":
+            sys.exit()
+        func, args = parse_command(command)
+        if func:
+            func(*args)
+        else:
+            print("Unknown command")
+
+if __name__ == '__main__':
+    main()
